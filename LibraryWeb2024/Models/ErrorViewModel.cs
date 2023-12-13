d90e3b4d1ff14b770462f61ccfc9a8342b6b5130
AMD <AMD@DESKTOP-LTLE68J>
2023-12-13 17:15:06 +0100

    Dodaj pliki projektów.

---
+++ LibraryWeb2024/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LibraryWeb2024.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
